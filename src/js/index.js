9b0ac7cd3d329863048fbb57aabe6219b46697db
gexiao <xiaoge740@gmail.com>
2024-06-05 14:35:32 +0800

    实现英雄的筛选

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,11 @@ const app = createApp({
   data() {
     return {
       heroList: [],
+      query:{
+        key:'dingwei',
+        value:0,
+      },
+      checked:0
     }
   },
   computed: {
@@ -16,6 +21,17 @@ const app = createApp({
     heroImage() {
       return (value) => `https://game.gtimg.cn/images/yxzj/img201606/heroimg/${value}/${value}.jpg`
     },
+    filterHeros() {
+      const { key, value } = this.query // 'pay_type' || 'dingwei'
+      if (key === 'pay_type') {
+        return this.heroList.filter((item) => item.pay_type === value)
+      } else if (key === 'dingwei') {
+        if (value === 0) {
+          return this.heroList
+        }
+        return this.heroList.filter((item) => item.hero_type === value || item.hero_type2 === value)
+      }
+    },
   },
   methods: {
     async getHeroList() {
@@ -26,11 +42,18 @@ const app = createApp({
       )
       this.heroList = data.data
     },
+    changeType(key, value) {
+      this.query.key = key
+      this.query.value = value
+      this.checked = value
+    },
   },
+
   // mounted 生命周期函数
   mounted() {
     // 异步请求数据
     this.getHeroList()
   },
+  
 })
 app.mount('#app')
